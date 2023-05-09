0dcfc4cc18b4dd2195baed6dd017563d88302213
Aang Anggriawan <aanganggriawan222@gmail.com>
2023-05-09 09:51:59 +0700

    Module-NodeJS-Biodata

---
+++ module/Tugas3.js
@@ -0,0 +1,7 @@
+
+const biodata = require('./obkTugas3');
+
+console.log("Nama : "+biodata.nama);
+console.log("Tempat Lahir : "+biodata.tempatLahir);
+console.log("Tanggal Lahir : "+biodata.tgLahir);
+console.log("Alamat : "+biodata.alamat);
---
+++ module/obkTugas3.js
@@ -0,0 +1,6 @@
+module.exports = {
+    nama : "Maman Budiman",
+    tempatLahir : "Cianjur",
+    tgLahir : "8 Agustus 2000",
+    alamat : "Jl. Neptunus No.90"
+}
